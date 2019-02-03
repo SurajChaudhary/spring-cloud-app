60452ffb3765d7da6c49745b978cfc493e46e73e
Suraj Chaudhary <surchaud@akamai.com>
2019-02-03 13:11:16 +0530

    Adding rooms api

--- src/main/java/com/devtalk/com/springcloudapp/controller/RoomController.java
+++ src/main/java/com/devtalk/com/springcloudapp/controller/RoomController.java
@@ -4,12 +4,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @RequestMapping("/roomservice")
 public class RoomController {
 
     @GetMapping("/hello")
-    public String hello(){
+    public String hello() {
         return "hello from roomService";
+    }
+
+    @GetMapping("/rooms")
+    public List<String> rooms() {
+        return Arrays.asList(new String[]{"room1", "room2", "room3"});
     }
 }
