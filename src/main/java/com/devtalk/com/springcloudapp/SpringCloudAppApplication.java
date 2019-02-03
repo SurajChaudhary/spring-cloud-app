756ffa5debb8971dae64f4fba9415d1af3873ef4
Suraj Chaudhary <surchaud@akamai.com>
2019-02-03 11:25:29 +0530

    initializing the repository

---
+++ src/main/java/com/devtalk/com/springcloudapp/SpringCloudAppApplication.java
@@ -0,0 +1,14 @@
+package com.devtalk.com.springcloudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudAppApplication.class, args);
+    }
+
+}
+
